767fb20d5bf2cd75f7806e1ce9e52de9b1926d20
Jorge <jorge.wellison@dce.ufpb.br>
2019-08-08 23:58:41 -0300

    tratamento de erros de http, usando catch

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import { MEAT_API } from "app/app.api";
 import { Http, ResponseOptions } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ErrorHandler } from "app/app.error-handler";
 
 @Injectable()
 export class RestaurantsService{
@@ -11,8 +13,9 @@ export class RestaurantsService{
 
   constructor(private http: Http){}
 
-  restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`)
-    .map(response => response.json());
+  restaurants(): Observable<Restaurant[]> { //Observable recebe do http
+    return this.http.get(`${MEAT_API}/restaurants`) //get envia pro http que faz o request ao DB
+    .map(response => response.json()) //Http recebe do DB
+    .catch(ErrorHandler.handleError);
   }
 }
