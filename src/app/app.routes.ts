e055b9e630b77438cbeb88aeac7791f1fcc1d1a8
Jorge <jorge.wellison@dce.ufpb.br>
2019-08-19 22:03:06 -0300

    Configurando a rota para o componente de compra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { OrderComponent } from './order/order.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
@@ -14,6 +15,7 @@ export const ROUTES: Routes = [
     children: [
       {path: '', redirectTo: 'menu', pathMatch: 'full'},
       {path: 'menu', component: MenuComponent},
-      {path: 'reviews', component: ReviewsComponent}
-    ]}
+      {path: 'reviews', component: ReviewsComponent},
+    ]},
+  {path: 'order', component: OrderComponent}
 ]
